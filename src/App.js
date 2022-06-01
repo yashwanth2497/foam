65ac3b2adcc97d106e4b67a8d0e7ef2ce5e60a23
yashwanth2497 <yashwanthakumar@gmail.com>
2022-06-01 12:24:38 +0530

    Hi

--- src/App.js
+++ src/App.js
@@ -2,24 +2,52 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
+  // JS ends
+  // JSX Starts
+  const names=["Kumar","Nithish","Aswin" ];
+  const students=[
+    {
+      name:"Kumar" ,
+      pic:"https://1.bp.blogspot.com/-W_7SWMP5Rag/YTuyV5XvtUI/AAAAAAAAuUQ/hm6bYcvlFgQqgv1uosog6K8y0dC9eglTQCLcBGAsYHQ/s880/Best-Profile-Pic-For-Boys%2B%25281%2529.jpg"
+    },
+    {
+      name:"Nithish" ,
+      pic:"https://bestprofilepictures.com/wp-content/uploads/2021/04/Awesome-Profile-Pic.jpg"
+    },
+    {
+      name:"Aswin" ,
+      pic:"https://bestprofilepictures.com/wp-content/uploads/2021/06/Anonymous-Profile-Photo-1.jpg"
+    }
+  ];
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+{/*       
+      <Message name="Kumar" pic="https://1.bp.blogspot.com/-W_7SWMP5Rag/YTuyV5XvtUI/AAAAAAAAuUQ/hm6bYcvlFgQqgv1uosog6K8y0dC9eglTQCLcBGAsYHQ/s880/Best-Profile-Pic-For-Boys%2B%25281%2529.jpg"/>
+      <Message name="Nithish" pic="https://bestprofilepictures.com/wp-content/uploads/2021/04/Awesome-Profile-Pic.jpg"/>
+      <Message name="Aswin" pic="https://bestprofilepictures.com/wp-content/uploads/2021/06/Anonymous-Profile-Photo-1.jpg"/> */}
+      {students.map((student)=>(
+        <Welcome name={student.name} pic={student.pic} />
+      ))}
     </div>
   );
+  // JSX ends
 }
 
 export default App;
+
+
+// function Message(props){
+//   console.log(props);
+  // return<>
+  // <img className="profile-pic" src={props.pic} alt={props.name}/>
+  // <h1>Hello, {props.name}</h1>
+  // </>
+// }
+
+function Welcome(props){
+  return<>
+  <img className="profile-pic" src={props.pic} alt={props.name}/>
+  <h1>Hello, {props.name}</h1>
+  </>
+
+}
